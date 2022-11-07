7c3bdb767654e42de896af4f2967646cbb25df62
Irmak Erdem <irmakerdem9@gmail.com>
2022-11-07 16:59:17 -0700

    Create TodoList component

---
+++ src/components/TodoList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Todo from '.Todo';
+
+const TodoList = ( {todos} ) => {
+  return (
+    <ul>
+      {todos.map(todo => {
+        {console.log(todos)}
+        <Todo 
+          key={todo.id} 
+          todo={todo}
+        />
+      })}
+    </ul>
+  )
+}
+
+export default TodoList;
